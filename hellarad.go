8c46104353c6f62056b819a7129ea6a619119d27
gyrospectre <you@example.com>
2021-12-29 11:32:14 +1100

    Extend structs

--- hellarad.go
+++ hellarad.go
@@ -15,10 +15,11 @@ type Result struct {
 }
 
 func (r *Result) Prettify() string {
+	var message string
 	if r.Success == true {
-		message := fmt.Sprintf("Details on %s from %s:\n%s", r.AttributeValue, r.Source, r.Message)
+		message = fmt.Sprintf("Details on %s from %s:\n%s", r.AttributeValue, r.Source, r.Message)
 	} else {
-		message := fmt.Sprintf("Failed to get info from %s! Error: %s", r.Source, r.Message)
+		message = fmt.Sprintf("Failed to get info from %s! Error: %s", r.Source, r.Message)
 	}
 
     return message
