b8ca76ca98a845325493a74b0853546cef04f7dd
gyrospectre <you@example.com>
2021-12-30 15:45:07 +1100

    Add SNS

--- conductor/main.go
+++ conductor/main.go
@@ -1,76 +1,106 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
     "encoding/json"
-	"errors"
-	"github.com/aws/aws-sdk-go/aws"
+    "errors"
+    "log"
+    "regexp"
+    "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/sfn"
-	"github.com/aws/aws-sdk-go/service/cloudformation"
-	"context"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/gyrospectre/hellarad"
+    "github.com/aws/aws-sdk-go/service/cloudformation"
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/gyrospectre/hellarad"
 )
 
-func getStackResourceArn(svc *cloudformation.CloudFormation, stackName string, resourceName string) (string, error) {
-	req := cloudformation.ListStackResourcesInput{
-		StackName: aws.String(stackName),
-	}
-	for {
-		resp, err := svc.ListStackResources(&req)
-		if err != nil {
-			return "", err
-		}
-		for _, s := range resp.StackResourceSummaries {
-			if *s.LogicalResourceId == resourceName {
-				return *s.PhysicalResourceId, nil
-			}
-		}
-		req.NextToken = resp.NextToken
-		if aws.StringValue(req.NextToken) == "" {
-			break
-		}
-	}
-	return "", errors.New("No matching stack resources found!")
+type AllPurposeAlert struct {
+    Message		string `json:"message"`		// Splunk
+    SearchName	string `json:"search_name"`
+    Alert		string `json:"alert"`		// OpsGenie
 }
 
-func HandleRequest(ctx context.Context, subject hellarad.Subject) (string, error) {
-	// Just for testing, generate some data
-	var inputList = []hellarad.Subject { 
-		hellarad.Subject {
-			IP: "202.92.65.254",
-		},
-		hellarad.Subject {
-			IP: "8.8.8.8",
-		},
-	}
-	inputJson, _ := json.Marshal(inputList)
+func getStackResourceArn(svc *cloudformation.CloudFormation, stackName string, resourceName string) (string, error) {
+    req := cloudformation.ListStackResourcesInput{
+        StackName: aws.String(stackName),
+    }
+    for {
+        resp, err := svc.ListStackResources(&req)
+        if err != nil {
+            return "", err
+        }
+        for _, s := range resp.StackResourceSummaries {
+            if *s.LogicalResourceId == resourceName {
+                return *s.PhysicalResourceId, nil
+            }
+        }
+        req.NextToken = resp.NextToken
+        if aws.StringValue(req.NextToken) == "" {
+            break
+        }
+    }
+    return "", errors.New("No matching stack resources found!")
+}
 
-	sesh := session.Must(session.NewSessionWithOptions(session.Options{
+func HandleRequest(ctx context.Context, snsEvent events.SNSEvent) (string, error) {
+    var inputList []hellarad.Subject
+
+    for _, record := range snsEvent.Records {
+        snsRecord := record.SNS
+
+        log.Printf("Processing message %s\n", snsRecord.MessageID)
+
+        var messageObject AllPurposeAlert
+        json.Unmarshal([]byte(snsRecord.Message), &messageObject)
+
+        var details string
+
+        if messageObject.SearchName != "" { 			// Source is Splunk
+            log.Println("Got Splunk alert")
+            details = messageObject.Message
+        } else {
+            log.Println("Got OpsGenie alert")
+            details = messageObject.Alert
+        }
+        log.Printf("%s\n", details)
+
+        re := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
+        submatchall := re.FindAllString(details, -1)
+        for _, address := range submatchall {
+            var subject = hellarad.Subject {
+                IP: address,
+            }
+            inputList = append(inputList, subject)
+        }
+    }
+    inputJson, _ := json.Marshal(inputList)
+
+    sesh := session.Must(session.NewSessionWithOptions(session.Options{
         SharedConfigState: session.SharedConfigEnable,
     }))
 
-	cfnsvc := cloudformation.New(sesh)
-	sfnArn, err := getStackResourceArn(cfnsvc, "hellarad", "IPLookupStateMachine")
+    cfnsvc := cloudformation.New(sesh)
+    sfnArn, err := getStackResourceArn(cfnsvc, "hellarad", "IPLookupStateMachine")
 
-	if err != nil {
-		return sfnArn, err
-	}
+    if err != nil {
+        return sfnArn, err
+    }
 
-	sfnsvc := sfn.New(sesh)
-	result, err := sfnsvc.StartExecution(&sfn.StartExecutionInput{
+    sfnsvc := sfn.New(sesh)
+    result, err := sfnsvc.StartExecution(&sfn.StartExecutionInput{
         StateMachineArn: 	&sfnArn,
-		Input: aws.String(string(inputJson)),
+        Input: aws.String(string(inputJson)),
     })
 
-	if err != nil {
-		fmt.Print(string(err.Error()))
-		return string(err.Error()), err
-	}
-	return result.GoString(), nil
+    if err != nil {
+        fmt.Print(string(err.Error()))
+        return string(err.Error()), err
+    }
+    return result.GoString(), nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+    lambda.Start(HandleRequest)
 }
--- functions/greynoise/main.go
+++ functions/greynoise/main.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-	"context"
-	"github.com/aws/aws-lambda-go/lambda"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"strings"
-	"github.com/gyrospectre/hellarad"
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "strings"
+    "github.com/gyrospectre/hellarad"
 )
 
 const (
-	Provider = "GreyNoise"
-	BaseURL = "https://api.greynoise.io/v3/community"
+    Provider = "GreyNoise"
+    BaseURL = "https://api.greynoise.io/v3/community"
 )
 
 type greynoiseResponse struct {
-	IP 				string 	`json:"ip"`
+    IP 				string 	`json:"ip"`
     Noise      		bool	`json:"noise"`
-	Riot			bool	`json:"riot"`
-	Classification	string 	`json:"classification"`
-	Name			string	`json:"name"`
-	Link			string 	`json:"link"`
-	LastSeen		string	`json:"last_seen"`
-	Message			string	`json:"message"`
+    Riot			bool	`json:"riot"`
+    Classification	string 	`json:"classification"`
+    Name			string	`json:"name"`
+    Link			string 	`json:"link"`
+    LastSeen		string	`json:"last_seen"`
+    Message			string	`json:"message"`
 }
 
 func HandleRequest(ctx context.Context, subject hellarad.Subject) (string, error) {
-	var result = hellarad.Result{Source: Provider, AttributeValue: subject.IP, Success: false}
+    var result = hellarad.Result{Source: Provider, AttributeValue: subject.IP, Success: false}
 
-	response, err := http.Get(fmt.Sprintf("%s/%s", strings.TrimSuffix(BaseURL, "/"), subject.IP))
+    response, err := http.Get(fmt.Sprintf("%s/%s", strings.TrimSuffix(BaseURL, "/"), subject.IP))
 
-	if err == nil {
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err == nil {
-			var responseObject greynoiseResponse
-			json.Unmarshal(responseData, &responseObject)
-			prettyresponse, _ := json.MarshalIndent(responseObject, "", "\t")
+    if err == nil {
+        responseData, err := ioutil.ReadAll(response.Body)
+        if err == nil {
+            var responseObject greynoiseResponse
+            json.Unmarshal(responseData, &responseObject)
+            prettyresponse, _ := json.MarshalIndent(responseObject, "", "\t")
 
-			result.Success = true
-			result.Message = string(prettyresponse)
-		} else {
-			fmt.Print("Error decoding response!")
-			fmt.Print(string(err.Error()))
-		}
-	} else {
-		fmt.Print("Error fetching data from API!")
-		fmt.Print(string(err.Error()))
-	}
+            result.Success = true
+            result.Message = string(prettyresponse)
+        } else {
+            fmt.Print("Error decoding response!")
+            fmt.Print(string(err.Error()))
+        }
+    } else {
+        fmt.Print("Error fetching data from API!")
+        fmt.Print(string(err.Error()))
+    }
 
-	if err != nil {
-		result.Message = string(err.Error())
-		return result.Prettify(), err
-	} else {
-		return result.Prettify(), nil
-	}
+    if err != nil {
+        result.Message = string(err.Error())
+        return result.Prettify(), err
+    } else {
+        return result.Prettify(), nil
+    }
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+    lambda.Start(HandleRequest)
 }
